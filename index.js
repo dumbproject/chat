423c3bf97aeae83ef3509c761aa188ac69230dae
zzivnuska <zzivnuska@gmail.com>
2021-10-25 05:32:16 -0700

    console log?

--- index.js
+++ index.js
@@ -13,22 +13,24 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
 
-    // io.emit('chat message', msg);((
+    io.emit('chat message', msg)
     console.log('message: ' + msg)
-  
-  });
-  // socket.onmessage = (msg) => {
-  //   log.append
-  //   console.log(data);
-  // };
+  })
 });
 
+// socket.onmessage = (msg) => {
+//   log.append
+//   console.log(data);
+// };
 
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
-  console.log(log)
+  // console.log(log)
 });
+
+
+// io.serverSideEmit("hello", "world");
 
 
 
